a67e6cee60fa33354bc3691a1c40075c20aec487
pavani <pavani.nandipati@finatext.com>
2020-12-30 19:45:36 +0000

    Used ImplicitWait

--- src/main/java/CarRegNumPage.java
+++ src/main/java/CarRegNumPage.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
@@ -25,11 +27,11 @@ public class CarRegNumPage extends ReadFiles {
     private static WebDriver webDriver;
     private String inputValue;
 
-    /**  - Test Initialisation
-     * @throws InterruptedException
+    /**
+     * Test Initialisation
      */
     @BeforeTest
-    public void initializeTest() throws InterruptedException {
+    public void initializeTest() {
 
         // Reads input file and gets all the Reg Pattern matched Car RegNums
         readInputFile();
@@ -41,7 +43,7 @@ public class CarRegNumPage extends ReadFiles {
 
         // Launches the site
         webDriver.get("https://cartaxcheck.co.uk");
-        Thread.sleep(120);
+        implicitWait(webDriver);
 
         // Waiting until to find the RegNum TextField
         WebDriverWait wait = new WebDriverWait(webDriver, 180);
@@ -66,10 +68,9 @@ public class CarRegNumPage extends ReadFiles {
     /**
      * Enters Car RegNumber
      * @param regNum
-     * @throws InterruptedException
      */
-    private void enterCarRegistrationNumber(String regNum) throws InterruptedException {
-        Thread.sleep(120);
+    private void enterCarRegistrationNumber(String regNum) {
+        implicitWait(webDriver);
         webDriver.findElement(By.id("vrm-input")).click();
         webDriver.findElement(By.id("vrm-input")).clear();
         webDriver.findElement(By.id("vrm-input")).sendKeys(regNum);
@@ -79,16 +80,14 @@ public class CarRegNumPage extends ReadFiles {
 
     /**
      * Clicks on 'Full History Check' button in the WebSite
-     * @throws InterruptedException
      */
-    public void clickFullHistoryCheckButton() throws InterruptedException {
-        Thread.sleep(120);
+    public void clickFullHistoryCheckButton() {
+        implicitWait(webDriver);
 
         // If the Size of the WebElements is greater than Zero then only clicks on the button
         if (webDriver.findElements(By.xpath("//form/a")).size() > 0) {
             webDriver.findElement(By.xpath("//form/a")).click();
             System.out.println(" -- Clicked on 'Full History Check' button -- ");
-            Thread.sleep(100);
         }
     }
 
@@ -96,10 +95,10 @@ public class CarRegNumPage extends ReadFiles {
      * Based on Input file gets 'RegNum' based on Reg Pattern and inputs to the Site
      * @param regNum
      * @return
-     * @throws InterruptedException
      */
-    public boolean getCarAttributesAndCompareWithOutputFile(String regNum) throws InterruptedException {
+    public boolean getCarAttributesAndCompareWithOutputFile(String regNum) {
         boolean check = false;
+        implicitWait(webDriver);
 
         // If the RegNum contains SPACES then joining it
         if (regNum.contains(" ")) {
@@ -107,25 +106,27 @@ public class CarRegNumPage extends ReadFiles {
             regNum = newReg[0] + newReg[1];
             System.out.println(" -- Removed spaces in RegNum :: " + regNum);
         }
-
         enterCarRegistrationNumber(regNum);
         clickFullHistoryCheckButton();
-        Thread.sleep(200);
+        implicitWait(webDriver);
 
         // If the RegNum matches then CarAttributes list will be passed to readOutFile. If it matches then returns true
         try {
-          if (webDriver.findElements(By.xpath("//*[@id=\"m\"]/div[2]/div/div/div[1]/div/p")).size() == 1) {
-            String carAttributes = webDriver.findElement(By.xpath("//*[@id=\"m\"]/div[2]/div/div/div[1]/div/p")).getText();
-            if (!(carAttributes.length() == 0)) {
-                System.out.println(" -- Car attributes :: " + carAttributes);
-                String carAttr = getCarAttributes(carAttributes);
-                check = readOutputFile(carAttr);
+            implicitWait(webDriver);
+            if (webDriver.findElements(By.xpath("//*[@id=\"m\"]/div[2]/div/div/div[1]/div/p")).size() == 1) {
+                System.out.println(" kdslfdf :: " + webDriver.findElement(By.xpath("//*[@id=\"m\"]/div[2]/div/div/div[1]/div/p")).getText());
+                String carAttributes = webDriver.findElement(By.xpath("//*[@id=\"m\"]/div[2]/div/div/div[1]/div/p")).getText();
+                if (!(carAttributes.length() == 0)) {
+                    System.out.println(" -- Car Attributes :: " + carAttributes);
+                    String carAttr = getCarAttributes(carAttributes);
+                    check = readOutputFile(carAttr);
+                }
             }
-          }
         } catch (Exception e) {
             // If the RegNum doesn't not match then CarAttributes list will not be displayed then returns false
-            Thread.sleep(150);
-            if (webDriver.findElements(By.xpath("//h5/span[contains(text(), 'Vehicle Not Found')]")).size() > 0) { }
+            if (webDriver.findElements(By.xpath("//h5/span[contains(text(), 'Vehicle Not Found')]")).size() > 0) {
+                System.out.println(" ***** Vehicle not found ***** ");
+            }
         }
         return check;
     }
@@ -136,10 +137,14 @@ public class CarRegNumPage extends ReadFiles {
      * @return
      */
     private String getCarAttributes(String originalStr) {
-        String split[] = originalStr.split(" ", 4);
+        String[] split = originalStr.split(" ", 4);
         String newSt = inputValue + "," + split[2] + "," + split[3].trim() + "," + split[0] + "," + split[1];
         System.out.println(" -- Car Attributes updated string :: " + newSt);
         return newSt;
+    }
+
+    private static void implicitWait(WebDriver webDriver) {
+        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     /**
@@ -156,29 +161,27 @@ public class CarRegNumPage extends ReadFiles {
 
     /**
      * Runs each test and returns result
-     * @throws InterruptedException
      */
     @Test
-    public void TestResults() throws InterruptedException {
+    public void TestResults() {
 
         System.out.println(" ************* TEST 1 ************ ");
         assertTrue(getCarAttributesAndCompareWithOutputFile("DN09HRM"));
 
         System.out.println(" ************* TEST 2 ************ ");
         webDriver.get(getUpdatedUrl("KT17DLX"));
-        Thread.sleep(120);
+        implicitWait(webDriver);
         assertTrue(getCarAttributesAndCompareWithOutputFile("KT17DLX"));
 
         System.out.println(" ************* TEST 3 ************ ");
         webDriver.get(getUpdatedUrl("SG18HTN"));
         webDriver.navigate().refresh();
-        Thread.sleep(120);
+        implicitWait(webDriver);
         assertTrue(getCarAttributesAndCompareWithOutputFile("SG18 HTN"));
 
         System.out.println(" ************* TEST 4 ************ ");
         webDriver.get(getUpdatedUrl("BW57BOW"));
         webDriver.navigate().refresh();
-        Thread.sleep(120);
         assertFalse(getCarAttributesAndCompareWithOutputFile("BW57 BOW"));
     }
 }
